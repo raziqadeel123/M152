e04260c49bd54de49baae20f19d1a939494b8d5e
raziq adeel <raziq.adeel7@gmail.com>
2022-12-04 20:49:32 +0100

    header styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/header/Header';
 
 function App() {
   return (
     <div className='App'>
-      <span>Testing the setup</span>
+      <Header />
     </div>
   );
 }
