8632cb2312e95657f57ffa01a974881b57f32e29
raziq adeel <raziq.adeel7@gmail.com>
2022-12-15 13:14:42 +0100

    still working on it

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,17 +5,17 @@ import './HeroStyle.css';
 
 const Hero = () => {
   return (
-    <section className='hero fd-r is-large ml-6 d-f '>
-      <div class='hero-body'>
-        <Typist className='blink hideWhenDone'>
-          <Typist.Delay ms={500} />
-          <p class='title-large'>Buy Now</p>
-          <Typist.Delay ms={1000} />
-          From 10+ Famouse Brands of the World
-        </Typist>
-      </div>
-      <div className='w50p mr-6 mt-6'>
-        <img src={bannerImg} alt='banner image' />
+    <section className='hero fd-c is-large ml-6 d-f '>
+      <div class='hero-body d-f fd-c '>
+        <p class='title-large'>Buy Now</p>
+        <p>
+          From 10+ Famouse Brands of the World, shipping internalay with one
+          week return Policy
+        </p>
+        <div className=' d-f '>
+          <button class='button is-warning mr-3 '>Get Started</button>
+          <button class='button is-warning '>Watch Now</button>
+        </div>
       </div>
     </section>
   );
