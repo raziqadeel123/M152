09245abedaec875c449e86f771e4a0d88883e0ce
raziq adeel <raziq.adeel7@gmail.com>
2022-12-18 15:14:50 +0100

    Change the Hero Section

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Typist from 'react-typist';
 import bannerImg from '../../assets/blob.svg';
+import { FaPlay } from 'react-icons/fa';
 import './HeroStyle.css';
 
 const Hero = () => {
   return (
     <section className='hero fd-c is-large ml-6 d-f '>
-      <div class='hero-body d-f fd-c '>
+      <div class='hero-body d-f fd-c f-sc '>
         <p class='title-large'>Buy Now</p>
         <p>
-          From 10+ Famouse Brands of the World, shipping internalay with one
-          week return Policy
+          Shoes From 10+ Famous Brands of the World, shipping internationally
+          and within one week return Policy
         </p>
         <div className=' d-f '>
           <button class='button is-warning mr-3 '>Get Started</button>
-          <button class='button is-warning '>Watch Now</button>
+          <button class='button is-warning  '>
+            Watch Now <FaPlay className='ml-2' />
+          </button>
         </div>
       </div>
     </section>
