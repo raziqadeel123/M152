283e6c6a0923c65bbdedbe2ad3817c81c6d77cab
raziq adeel <raziq.adeel7@gmail.com>
2022-12-22 13:03:18 +0100

    video page

--- src/Pages/Video.jsx
+++ src/Pages/Video.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import videobg from '../assets/Videos/bg.mp4';
+import Header from '../Components/header/Header';
 import './pages.css';
 
 const Video = () => {
   return (
-    <div className='main-section'>
-      <div className='overlay'></div>
-      <video src={videobg} autoPlay loop muted></video>
-      <div className='video-content'>
-        <h4>Move with us</h4>
-        <p>faster and safe </p>
+    <>
+      <Header />
+      <div className='main-section'>
+        <div className='overlay'></div>
+        <video src={videobg} autoPlay loop muted></video>
+        <div className='video-content'>
+          <h4>Move with us</h4>
+          <p>faster and safe </p>
+          <button className='button is-link'>Explore</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
