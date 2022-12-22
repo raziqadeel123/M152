da9b4850a796edde7a3bed1c532d7c92e030ad1d
raziq adeel <raziq.adeel7@gmail.com>
2022-12-22 10:29:37 +0100

    added the video background

--- src/Pages/Video.jsx
+++ src/Pages/Video.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import videobg from '../assets/Videos/bg.mp4';
+import './pages.css';
 
 const Video = () => {
-  return <div>Video</div>;
+  return (
+    <div className='main-section'>
+      <div className='overlay'></div>
+      <video src={videobg} autoPlay loop muted></video>
+      <div className='video-content'>
+        <h4>Move with us</h4>
+        <p>faster and safe </p>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
